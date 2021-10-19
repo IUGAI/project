d98cb111c04bb63d3711507b6a1fbc3184b7e0ca
ALEX <yugaialex9813@kbu.ac.kr>
2021-10-19 18:07:46 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+console.log(4/0)
+let person = {name :"John"
+              ,age : 25
+              ,isMarried : false
+
+};
+console.log(person.name)
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]); 
+let answer = prompt("есть ли вам 18?","да");
+console.log(typeof(arr));
+console.log("2" == 2);
